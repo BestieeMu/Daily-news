a7fe9f24bdddeb12b1b2d03c11d3ff517e94266f
BestieeMu <93823200+BestieeMu@users.noreply.github.com>
2023-03-11 00:16:48 +0000

    test

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row bg-green-400  gap-1 w-full md:w-10/12 mt-10">
-        <div class="parent rounded overflow-hidden w-full">
+        <div class="parent rounded overflow-hidden h-96 w-full">
 
           <div class="div1  " style={{
               backgroundImage: `url(${'https://images.pexels.com/photos/1464210/pexels-photo-1464210.jpeg?auto=compress&cs=tinysrgb&w=600'})`,
